6a6be04fbec9278272e010cfe0d6c37aed8cb030
edobasslife <eduardo11diaz@gmail.com>
2021-03-27 11:48:14 -0400

    se eliminaron parametros del constructor de Outlander

--- src/com/edobass/Outlander.java
+++ src/com/edobass/Outlander.java
@@ -3,7 +3,7 @@ package com.edobass;
 public class Outlander extends Car {
     private int roudServiceMonths;
 
-    public Outlander(int wheels, int doors, int gears, boolean isManual, int roudServiceMonths) {
+    public Outlander(int roudServiceMonths) {
         super("Outlander","4WD", 5, 5, 6, false);
         this.roudServiceMonths = roudServiceMonths;
     }
