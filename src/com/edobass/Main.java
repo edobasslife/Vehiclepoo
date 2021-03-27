e6a5d9faa2e662a8418eb8b61b1e968b86417b92
edobasslife <eduardo11diaz@gmail.com>
2021-03-27 08:58:07 -0400

    mi primer commit

---
+++ src/com/edobass/Main.java
@@ -0,0 +1,8 @@
+package com.edobass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/edobass/Vehicle.java
@@ -0,0 +1,57 @@
+package com.edobass;
+
+/*
+// Desafío.
+         // Comience con una clase base de Vehículo, luego cree una clase Coche que herede de esta clase base.
+         // Finalmente, cree otra clase, un tipo específico de Coche que herede de la clase Coche.
+         // Debería poder conducir con la mano, cambiar de marcha y moverse (velocidad en otras palabras).
+         // Deberá decidir dónde colocar el estado y los comportamientos apropiados (campos y métodos).
+         // Como se mencionó anteriormente, se deben incluir cambios de marcha, aumento / disminución de velocidad.
+         // Para su tipo específico de vehículo, querrá agregar algo específico para ese tipo de automóvil.
+You should be able to hand steering, changing gears, and moving (speed in other words).
+ */
+
+public class Vehicle {
+    private int name;
+    private int size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(int name, int size) {
+
+        this.name = name;
+        this.size = size;
+
+        this.currentDirection = 0;
+        this.currentVelocity = 0;
+    }
+
+    public void steer(int direction) {
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at" + currentDirection + "degress");
+    }
+
+    public void move(int velocity, int direction) {
+        currentVelocity = velocity;
+        currentDirection = direction;
+        System.out.println("Vehicle move(): Moving at " + currentVelocity + "in direction " + currentDirection);
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setCurrentVelocity(int currentVelocity) {
+        this.currentVelocity = currentVelocity;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+}
+
